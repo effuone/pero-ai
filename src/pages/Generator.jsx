440b1c88b363a943494f0ed4f2e9547a149ca5ae
effuone <effuone>
2022-08-10 12:20:51 +0600

    Generation page implemented

--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -5,14 +5,21 @@ const Generator = () => {
     return (
         <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
             <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="name@example.com" />
+                <Form.Group className="mb-3">
+                    <Form.Label>Заголовок</Form.Label>
+                    <Form.Control type="text" placeholder="nfactorial demo day 2022" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                <Form.Group className="mb-3">
+                    <Form.Label>Ключевые слова</Form.Label>
+                    <Form.Control type="text" placeholder="IT, веб-приложения, инновации" />
                 </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Описание аудиотрии</Form.Label>
+                    <Form.Control as="textarea" rows={3} placeholder="tech энтузиазисты, предприниматели"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Сгенерерировать
+                </Button>
             </Form>
         </div>
     )
